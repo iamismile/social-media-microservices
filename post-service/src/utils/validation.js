867cdcd4520348cd5f40281492a866adabe9576d
Ismile Hossain <iamismile.dev@gmail.com>
2026-05-15 15:59:39 +0600

    add media ids in post creation

--- post-service/src/utils/validation.js
+++ post-service/src/utils/validation.js
@@ -3,6 +3,7 @@ const Joi = require("joi");
 const validateCreatePost = (data) => {
   const schema = Joi.object({
     content: Joi.string().min(3).max(500).required(),
+    mediaIds: Joi.array().items(Joi.string().hex().length(24)).optional(),
   }).required();
 
   return schema.validate(data);
